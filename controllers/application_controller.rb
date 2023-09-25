7f51e037cb5c80549d23ad8cb12e92c520f72170
Raj18101999 <143397306+Raj18101999@users.noreply.github.com>
2023-09-25 15:14:27 +0530

    Add files via upload

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,22 +1,2 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_login!
-	before_action :configure_permitted_parameters, if: :devise_controller?
-  
-
-  def after_sign_in_path_for(login)
-    if current_login.role == 'admin'
-      root_path
-    elsif current_login.role == 'user'
-      students_path
-    else
-      root_path
-    end
-  end
-
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
-  end
-
 end
